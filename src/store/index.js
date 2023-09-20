a321244229f8792a3bc40d3b39ed680144390f21
scontt <deelog1233223@gmail.com>
2023-09-20 19:13:26 +0700

    vuex

---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+const store = new Vuex.Store({
+    state: {
+        groups: 1
+    },
+    getters: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    },
+    modules: {
+
+    }
+})
+
+// export default createStore({
+    
